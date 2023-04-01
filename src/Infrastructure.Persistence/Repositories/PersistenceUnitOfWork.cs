96e9a276b39838aaa18bfc32a2b0bce0883f0994
habibur.softifybd <habibur.softifybd@gmail.com>
2023-04-01 15:12:58 +0600

    initial commit

---
+++ src/Infrastructure.Persistence/Repositories/PersistenceUnitOfWork.cs
@@ -0,0 +1,67 @@
+﻿using Core.Domain.Persistence.Contracts;
+using Core.Domain.Persistence.Entities;
+using Infrastructure.Persistence.Context;
+using LinqToDB.Data;
+using LinqToDB.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    public class PersistenceUnitOfWork : IPersistenceUnitOfWork
+    {
+        public IRepositoryAsync<Audit> Audit { get; }
+        public IRepositoryAsync<Brand> Brand { get; }
+
+        private readonly AppDbContext _dbContext;
+        private bool _disposed;
+
+        public PersistenceUnitOfWork(AppDbContext appDbContext,
+            IRepositoryAsync<Audit> auditRepository, IRepositoryAsync<Brand> brandRepository)
+        {
+            _dbContext = appDbContext;
+            Audit = auditRepository;
+            Brand = brandRepository;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public DataConnection Linq2Db => _dbContext.CreateLinqToDbConnection();
+
+
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _dbContext.SaveChangesAsync();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed && disposing) _dbContext.Dispose();
+            _disposed = true;
+        }
+
+        public async Task<IDbContextTransaction> BeginTranscationAsync()
+        {
+            return await _dbContext.Database.BeginTransactionAsync();
+        }
+
+        public async Task CommitTransactionAsync()
+        {
+            await _dbContext.Database.CommitTransactionAsync();
+        }
+
+        public async Task RollbackTransactionAsync()
+        {
+            await _dbContext.Database.RollbackTransactionAsync();
+        }
+    }
+}
---
+++ src/Web.Api/Program.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.OpenApi.Models;
+using Serilog;
+using Swashbuckle.AspNetCore.JsonMultipartFormDataSupport.Extensions;
+using Swashbuckle.AspNetCore.JsonMultipartFormDataSupport.Integrations;
+using Web.Framework.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+var services= builder.Services;
+var connectionString = builder.Configuration.GetConnectionString("ConnStr");
+// Add services to the container.
+services.AddCors();
+services.AddAutoMapper();
+services.AddFramework(builder.Configuration,connectionString);
+services.AddControllers();
+services.AddApiVersioningExtension();
+services.AddJsonMultipartFormDataSupport(JsonSerializerChoice.Newtonsoft);
+services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Web.Api", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Web.Api v1"));
+app.UseCors(builder => builder
+    .AllowAnyOrigin()
+    .AllowAnyHeader()
+    .AllowAnyMethod());
+app.UseRouting();
+
+app.UseAuthorization();
+app.UseStaticFiles();
+app.UseApiErrorHandlingMiddleware();
+//app.UseSerilogRequestLogging();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+app.UseHttpsRedirection();
+app.MapControllers();
+var isDevelopment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == Environments.Development;
+var appSettingFile = isDevelopment ? $"appsettings.Development.json" : "appsettings.json";
+var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile(appSettingFile, optional: true, reloadOnChange: true)
+                .Build();
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .CreateLogger();
+app.Run();
